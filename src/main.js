681458ce1a64741318ad777cbf07823eb34a6bc7
tomq <512309244@qq.com>
2020-07-12 13:28:54 +0800

    login code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router.js'
 
-Vue.config.productionTip = false
+import './plugins/element.js'
 
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+
+import axios from 'axios'
+Vue.config.productionTip = false;
+
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+// 挂在到Vue实例，后面可通过this调用
+Vue.prototype.$http = axios
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Login from './components/Login.vue'
+import Home from './components/Home.vue'
+
+Vue.use(Router);
+
+
+var router=  new Router({
+    routes:[
+        {path:'/',redirect:'/login'},
+        {path:'/login',component:Login},
+        {path:'/home',component:Home},
+    ]
+})
+
+// 挂载路由导航守卫,to表示将要访问的路径，from表示从哪里来，next是下一个要做的操作 next('/login')强制跳转login
+router.beforeEach((to, from, next) => {
+    // 访问登录页，放行
+    if (to.path === '/login') return next()
+    // 获取token
+    const tokenStr = window.sessionStorage.getItem('token')
+    // 没有token, 强制跳转到登录页
+    if (!tokenStr) return next('/login')
+    next()
+  })
+  
+export default router
